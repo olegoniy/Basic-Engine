1b3aaf4edbf737de02bd89d684acf8fe1ef97553
Oleg Karpenko <olkieah1@gmail.com>
2025-06-17 16:31:50 +0200

    Empty window

---
+++ README.md
@@ -0,0 +1,2 @@
+# Basic-Engine
+A basic engine wriiten with Vulkan and C++ in order to learn them.
---
+++ lve_window.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+namespace lve {
+	
+	class LveWindow{
+		
+		public:
+			LveWindow(int w, int h, std::string name); 
+			~LveWindow();
+			
+			bool shouldClose() {return glfwWindowShouldClose(window); }
+			
+		private:
+		
+		void initWindow();
+		
+		const int width;
+		const int height;
+		
+		std::string windowName;
+		GLFWwindow *window;
+	};
+}
