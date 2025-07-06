4e11884960f22958366fd73f1f31552b9ea886c4
Oleg Karpenko <olkieah1@gmail.com>
2025-07-06 18:40:02 +0200

    Problem with validation layers

--- first_app.hpp
+++ first_app.hpp
@@ -2,19 +2,27 @@
 
 #include "lve_window.hpp"
 #include "lve_pipeline.hpp"
+#include "lve_device.hpp"
 
-namespace lve {
-	
-	class FirstApp{
-		
-		public:
-			static constexpr int WIDTH = 800;
-			static constexpr int HEIGHT = 600;
-			
-			void run();
-		
-		private:
-			LveWindow lveWindow{WIDTH, HEIGHT, "Hi there <3"};
-			LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv"};
-	}; 
+namespace lve
+{
+
+	class FirstApp 
+	{
+
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
+
+		void run();
+
+	private:
+		LveWindow lveWindow{WIDTH, HEIGHT, "Hi there <3"};
+		LveDevice lveDevice{lveWindow};
+		LvePipeline lvePipeline{
+			lveDevice,
+			"shaders/simple_shader.vert.spv",
+			"shaders/simple_shader.frag.spv",
+			LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+	};
 }
--- lve_pipeline.cpp
+++ lve_pipeline.cpp
@@ -4,35 +4,64 @@
 #include <fstream>
 #include <stdexcept>
 
-namespace lve {
-	
-	LvePipeline::LvePipeline(const std::string& vertFilepath, const std::string& fragFilepath){
-		createGraphicsPipeline(vertFilepath, fragFilepath);
+namespace lve
+{
+
+	LvePipeline::LvePipeline(
+		LveDevice& device,
+		const std::string &vertFilepath,
+		const std::string &fragFilepath,
+		const PipelineConfigInfo &configInfo) : lveDevice{device} {
+		createGraphicsPipeline(vertFilepath, fragFilepath, configInfo);
 	}
-	
-	std::vector<char> LvePipeline::readFile(const std::string& filepath) {
-		
-		std::ifstream file{filepath, std::ios::ate | std::ios::binary};
-		
-		if (!file.is_open()){
-			throw std::runtime_error("Failed to open file:" + filepath + '\n');
-			
-		}
-		
-		size_t fileSize = static_cast<size_t>(file.tellg());
-		std::vector<char> buffer(fileSize);
-		
-		file.seekg(0);
-		file.read(buffer.data(), fileSize);
-		
-		file.close();
-		return buffer;
+
+    PipelineConfigInfo LvePipeline::defaultPipelineConfigInfo(uint32_t width, uint32_t height)
+    {
+        PipelineConfigInfo configInfo{};
+
+		return configInfo;
+    }
+
+    std::vector<char> LvePipeline::readFile(const std::string &filepath)
+    {
+
+        std::ifstream file{filepath, std::ios::ate | std::ios::binary};
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error("Failed to open file:" + filepath + '\n');
+        }
+
+        size_t fileSize = static_cast<size_t>(file.tellg());
+        std::vector<char> buffer(fileSize);
+
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+
+        file.close();
+        return buffer;
+    }
+
+void LvePipeline::createGraphicsPipeline(
+			const std::string &vertFilepath,
+			const std::string &fragFilepath,
+			const PipelineConfigInfo &configInfo)
+{
+	auto vertCode = readFile(vertFilepath);
+	auto fragCode = readFile(fragFilepath);
+	std::cout << "Vertex Shader Code Size: " << vertCode.size() << '\n';
+	std::cout << "Fragment Shader Code Size: " << fragCode.size() << '\n';
+}
+
+void LvePipeline::createShaderModule(const std::vector<char> &code, VkShaderModule *shaderModule)
+{
+	VkShaderModuleCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.codeSize = code.size();
+	createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+	if (vkCreateShaderModule(lveDevice.device(), &createInfo, nullptr, shaderModule) != VK_SUCCESS){
+		throw std::runtime_error("Failed to create shader module");
 	}
-	
-	void LvePipeline::createGraphicsPipeline(const std::string& vertFilepath, const std::string& fragFilepath) {
-		auto vertCode = readFile(vertFilepath);
-		auto fragCode = readFile(fragFilepath);
-		std::cout << "Vertex Shader Code Size: " << vertCode.size() << '\n';
-		std::cout << "Fragment Shader Code Size: " << fragCode.size() << '\n';
-	}
+}
 }
